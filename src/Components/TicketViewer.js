c8105e57a9a46b8ae2ee666230fdd9cfbf8bcb86
Sanket Sapkal <sanket@cursorinsight.com>
2021-11-30 22:18:57 -0800

    Added page navigation using buttons.

--- src/Components/TicketViewer.js
+++ src/Components/TicketViewer.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Card, Grid, Label } from 'semantic-ui-react';
+import React from 'react';
+import { Button, Card, Grid, Label } from 'semantic-ui-react';
 
 export default class TicketViewer extends React.Component {
     constructor(props) {
@@ -14,6 +14,9 @@ export default class TicketViewer extends React.Component {
             previousEnd: true,
             nextEnd: false
         };
+
+        this.getNextPage = this.getNextPage.bind(this);
+        this.getPreviousPage = this.getPreviousPage.bind(this);
     }
 
     componentDidMount() {
@@ -22,7 +25,7 @@ export default class TicketViewer extends React.Component {
             .then(responseUnParsed => responseUnParsed.json())
             .then(response => {
                 // TODO: Check if the tickets list is empty, if it is then error
-                // has occured on server side
+                // has occurred on server side
                 this.setState({
                     page: 1,
                     tickets: response,
@@ -39,13 +42,69 @@ export default class TicketViewer extends React.Component {
                 this.setState({
                     count: response.ticketCount,
                     error: false,
-                    fetching: false
+                    fetching: false,
+                    previousEnd: true
                 })
+
+                if (this.state.count < this.state.page * 25) {
+                    this.setState({nextEnd: true});
+                }
             })
             .catch(error => {
                 console.log(error)
             })
     }
+
+    getNextPage(_event, _data) {
+        var newPage = this.state.page + 1;
+        // Fetch tickets for the new page number.
+        fetch('http://localhost:3000/ticketsOnPage/' + newPage)
+            .then(responseUnParsed => responseUnParsed.json())
+            .then(response => {
+                // TODO: Check if the tickets list is empty, if it is then error
+                // has occurred on server side
+                this.setState({
+                    page: newPage,
+                    tickets: response,
+                    error: false,
+                    previousEnd: false
+                })
+
+                // Disable next button on last page
+                if (this.state.count < this.state.page * 25) {
+                    this.setState({nextEnd: true});
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
+    getPreviousPage(_event, _data) {
+        var newPage = this.state.page - 1;
+        // Fetch tickets for the new page number.
+        fetch('http://localhost:3000/ticketsOnPage/' + newPage)
+            .then(responseUnParsed => responseUnParsed.json())
+            .then(response => {
+                // TODO: Check if the tickets list is empty, if it is then error
+                // has occurred on server side
+                this.setState({
+                    page: newPage,
+                    tickets: response,
+                    error: false,
+                    nextEnd: false
+                })
+
+                // Disable previous button on first page
+                if (this.state.page === 1) {
+                    this.setState({previousEnd: true});
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
 
     renderTickets() {
         const tickets = this.state.tickets.map(ticket =>(
@@ -79,19 +138,59 @@ export default class TicketViewer extends React.Component {
 
     render() {
         return (
-            <Grid centered>
-                <Grid.Column width="14">
-                    <Grid.Row>
-
-                    </Grid.Row>
-                    <Grid.Row>
-                        {this.renderTickets()}
-                    </Grid.Row>
-                    <Grid.Row>
-
-                    </Grid.Row>
+            <>
+            <Grid>
+                <Grid.Column floated="right">
+                    <Button
+                        icon
+                        disabled={this.state.fetching || this.state.previousEnd}
+                        color="blue"
+                        onClick={this.getPreviousPage}
+                    >
+                        Previous
+                    </Button>
+                </Grid.Column>
+                <Grid.Column><h2>{this.state.page}</h2></Grid.Column>
+                <Grid.Column floated="left">
+                    <Button
+                        icon
+                        disabled={this.state.fetching || this.state.nextEnd}
+                        color="blue"
+                        onClick={this.getNextPage}
+                    >
+                        Next
+                    </Button>
                 </Grid.Column>
             </Grid>
+            <Grid centered>
+                <Grid.Column width="14">
+                    {this.renderTickets()}
+                </Grid.Column>
+            </Grid>
+            <Grid>
+                <Grid.Column floated="right">
+                    <Button
+                        icon
+                        disabled={this.state.fetching || this.state.previousEnd}
+                        color="blue"
+                        onClick={this.getPreviousPage}
+                    >
+                        Previous
+                    </Button>
+                </Grid.Column>
+                <Grid.Column><h2>{this.state.page}</h2></Grid.Column>
+                <Grid.Column floated="left">
+                    <Button
+                        icon
+                        disabled={this.state.fetching || this.state.nextEnd}
+                        color="blue"
+                        onClick={this.getNextPage}
+                    >
+                        Next
+                    </Button>
+                </Grid.Column>
+            </Grid>
+            </>
         );
     }
 }
